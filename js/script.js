b7cfb253ace98db9a65130f5e9e556b1405ab1a4
Emil arrenius <Nils-Emil1337@hotmail.se>
2023-02-23 14:24:24 +0100

    aa

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log(document.getElementById('name').value= 'Anna karlsson')
+console.log(document.getElementById('email').value= 'Anna karlsson')
+lol = "5"
+console.log(lol)
+
+let firstName = "Hans"
+var kek = 1
+console.log(kek)
