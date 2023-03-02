113c951adc95c52718fd4cdf4c4c844d152430df
Emil arrenius <Nils-Emil1337@hotmail.se>
2023-03-02 12:52:20 +0100

    jj

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
+const names = document.getElementById('name')
+const email = document.getElementById('email')
+const comments = document.getElementById('comments')
 
+const form = document.getElementById('form')
 
 
 
@@ -7,12 +11,14 @@
 const validateName = (e) => {
   const regEx = /^[A-Z]([a-zA-Z\u00C0-\u017F]+(([' -][a-zA-Z])?[a-zA-Z]*)){1,30}$/
   if (!regEx.test(e.target.value)) {
-    document.getElementById("error").innerText = "Fel"
+    document.getElementById("error").innerText = "Invalid Name!"
+    document.getElementById('name-message').innerText = 'Your name has to start with uppercase'
     
       console.log("firstname not valid")
       return false
   }
   document.getElementById("error").innerText = ""
+  document.getElementById('name-message').innerText = ""
   
       
   console.log("firstname valid")
@@ -25,13 +31,40 @@ const validateEmail = (e) => {
   
 
   if (!regEx.test(e.target.value)) {
-    document.getElementById("error-email").innerText = "Fel"
+    document.getElementById("error-email").innerText = "Invalid Email!"
     
       console.log("firstname not valid")
       return false
   }
-  document.getElementById("error").innerText = ""
+  document.getElementById("error-email").innerText = ""
   console.log("firstname valid")
   return true
 
 }
+const validateComments = (e) => {
+  const regEx = /^([a-zA-Z\u00C0-\u017F]+(([' -][a-zA-Z])?[a-zA-Z]*)){1,30}$/
+
+  if (!regEx.test(e.target.value)) {
+    document.getElementById("error-comments").innerText = "Please write your comments"
+    
+      console.log("firstname not valid")
+      return false
+  }
+  document.getElementById("error-comments").innerText = ""
+  console.log("firstname valid")
+  return true
+}
+
+
+form.addEventListener('submit', (e) =>  {
+  let messages = []
+  if(names.value === '' || email.value === '' || comments.value === '')
+  messages.push('')
+  
+  if (messages.length > 0){
+    e.preventDefault()
+
+  }
+
+
+})
